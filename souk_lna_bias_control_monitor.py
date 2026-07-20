c9268776f30edbc075d5ac501e54537b9a39315b
WeiDaZhang <weida.zhang@outlook.com>
2026-07-20 15:25:26 +0200

    diable channel 9

--- souk_lna_bias_control_monitor.py
+++ souk_lna_bias_control_monitor.py
@@ -600,7 +600,7 @@ async def main():
         "--channels",
         type=int,
         nargs="+",
-        default=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        default=[1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14],  # 9 disabled
         help="List of LNA channel indices to monitor, starting index 1",
     )
     parser.add_argument(
